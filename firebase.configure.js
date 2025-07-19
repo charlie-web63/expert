f296f88ce28ae16c293095f09854ab394ca681e5
Charlie <charliederry800@gmail.com>
2025-07-20 02:09:58 +0300

    Create firebase.configure.js

---
+++ firebase.configure.js
@@ -0,0 +1,11 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyB5_zBUs_3V9M0DyVuN87HPPLwkG1C9EdU",
+  authDomain: "austine-food-show-website.firebaseapp.com",
+  projectId: "austine-food-show-website",
+  storageBucket: "austine-food-show-website.firebasestorage.app",
+  messagingSenderId: "29063789827",
+  appId: "1:29063789827:web:fd9b6f9139313f579cbd36"
+};
+const app = initializeApp(firebaseConfig);
